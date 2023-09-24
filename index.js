d0193b28dca998c7526df01e95fc673774d5e7e5
Pai <paaai@icloud.com>
2023-09-24 13:39:49 -0600

    CarService.js and Index.js modified to fit the cloud solution

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-// Requires
 const express = require('express');
 const cors = require('cors');
 const rateLimit = require('express-rate-limit');
+const { connectToDatabase } = require('./src/service/carService'); // Import the connectToDatabase function
+
 require('dotenv').config();
 
 const carController = require('./src/controller/carController');
@@ -15,17 +16,25 @@ const limiter = rateLimit({
     message: 'Too many requests from this IP, please try again later.',
 });
 
-
 // Middleware
-app.use(limiter);
 app.use(cors());
+app.use(limiter);
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ message: 'Internal Server Error' });
 });
 
+// Connect to the MongoDB database
+connectToDatabase()
+    .then(() => {
+        // Routes
+        app.use('/api', carController);
 
-app.use('/api', carController);
-app.listen(port || 5000, () => {
-    console.log('listening on port' + ' ' + port)
-})
+        // Start the Express app
+        app.listen(port || 5000, () => {
+            console.log('Server is running on port', port);
+        });
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+    });
--- src/service/carService.js
+++ src/service/carService.js
@@ -1,42 +1,45 @@
 const { MongoClient, ObjectId } = require('mongodb');
 require('dotenv').config();
 
-const uri = process.env.CONNECTION_STRING
+const uri = process.env.CONNECTION_STRING;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function connectToDatabase() {
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+        throw error;
+    }
+}
 
 async function getCars() {
     try {
-        await client.connect();
         const db = client.db('main');
         const carsCollection = db.collection('cars');
-
         const cars = await carsCollection.find({}).toArray();
         return cars;
     } catch (error) {
         console.error('Error fetching cars:', error);
         throw error;
-    } finally {
-        client.close();
     }
 }
 
 async function getCarById(carId) {
     try {
-        await client.connect();
         const db = client.db('main');
         const carsCollection = db.collection('cars');
-
         const car = await carsCollection.findOne({ _id: ObjectId(carId) });
         return car;
     } catch (error) {
         console.error('Error fetching car by ID:', error);
         throw error;
-    } finally {
-        client.close();
     }
 }
 
 module.exports = {
+    connectToDatabase,
     getCars,
     getCarById
 };
